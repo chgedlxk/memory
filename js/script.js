93f780b0fb10f824ab72ef73cf65dc0e05d2a40d
chgedlxk <chgedlxk@gmail.com>
2018-09-03 08:20:40 -0400

    Create array of image objects

---
+++ js/script.js
@@ -0,0 +1,66 @@
+// Card data
+
+const cardsArray = [
+	{
+		'name': 'shell',
+		'img': 'img/blueshell.png',
+	},	
+
+	{
+		'name': 'start',
+		'img': 'img/star.png',
+	},	
+
+	{
+		'name': 'bobomb',
+		'img': 'img/bobomb.png' ,
+	},	
+
+	{
+		'name': 'mario',
+		'img': 'img/mario.png',
+	},	
+
+	{
+		'name': 'luigi',
+		'img': 'img/luigi.png',
+	},	
+
+	{
+		'name': 'peach',
+		'img': 'img/peach.png',
+	},	
+
+	{
+		'name': '1up',
+		'img': 'img/1up.png',
+	},	
+
+	{
+		'name': 'mushroom',
+		'img': 'img/mushroom.png',
+	},	
+
+	{
+		'name': 'thwomp',
+		'img': 'img/thwomp.png',
+	},	
+
+	{
+		'name': 'bulletbill',
+		'img': 'img/bulletbill.png',
+	},	
+
+	{
+		'name': 'coin',
+		'img': 'img/coin.png',
+	},	
+
+	{
+		'name': 'boomba',
+		'img': 'img/boomba.png',
+	},	
+];
+
+const game = document.getElementById('game');
+
