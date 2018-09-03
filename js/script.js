214b0d553cac40aaa2b9b022b69222c604846093
chgedlxk <chgedlxk@gmail.com>
2018-09-03 10:24:44 -0400

    Add delay to selections

--- js/script.js
+++ js/script.js
@@ -93,17 +93,59 @@ gameGrid.forEach(item => {
 	const card = document.createElement('div');
 	card.setAttribute('class', 'card');
 	card.dataset.name = item.name;
+
+	
 	card.style.backgroundImage = `url(${item.img})`;
 	grid.appendChild(card);
 })
 
 let count = 0;
-grid.addEventListener('click', function(event) {
+let firstGuess = '';
+let secondGuess = '';
+let previousTarget = null;
+let delay = 1200;
 
+// Add match CSS
+const match = () => {
+	var selected = document.querySelectorAll('.selected');
+	selected.forEach(card => {
+		card.classList.add('match');
+	});
+}
+
+grid.addEventListener('click', function(event) {
 	let clicked = event.target;
-	if (clicked.nodeName === 'SECTION') { return; }
+	if (clicked.nodeName === 'SECTION' || clicked == previousTarget) { return; }
 	if (count < 2) {
 		count++;
-		clicked.classList.add('selected');
+		if (count === 1) {
+			firstGuess = clicked.dataset.name;
+			clicked.classList.add('selected');
+		} else {
+			secondGuess = clicked.dataset.name;
+			clicked.classList.add('selected');
+		}
+
+		if (firstGuess !== '' && secondGuess !== '') {
+			if (firstGuess === secondGuess) {
+				setTimeout(match, delay);
+				setTimeout(resetGuesses, delay);
+			} else {
+				setTimeout(resetGuesses, delay);
+			}
+		}
+		// Set previous target to clicked
+		previousTarget = clicked;
 	}
 });
+
+const resetGuesses = () => {
+	firstGuess = '';
+	secondGuess = '';
+	count = 0;
+	var selected = document.querySelectorAll('.selected');
+	selected.forEach(card => {
+		card.classList.remove('selected');
+	});
+};
+
